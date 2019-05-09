62032aedb25f3ad399b12689736a0153fde79150
unknown <malinduruwantha@gmail.com>
2019-05-09 18:53:18 +0530

    class components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greeting from './components/greeting'
+import Hello from './components/hello'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Greeting></Greeting>
+        <Hello></Hello>
         <a
           className="App-link"
           href="https://reactjs.org"
