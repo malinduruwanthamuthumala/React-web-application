091e312f24c596d038534203d0930b617168deb4
unknown <malinduruwantha@gmail.com>
2019-06-04 13:41:56 +0530

    making a call back function

--- src/components/counter.js
+++ src/components/counter.js
@@ -10,7 +10,7 @@ class counter extends Component{
     increment(){
         this.setState({
             count:this.state.count+1
-        })
+        },()=>{console.log(this.state.count)})
     }
     render(){
         return(
